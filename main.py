6a87b6ce2b668e3169935279801ecaffe5d4c7b7
Kyle MacFarland <kylejmacfarland@gmail.com>
2025-10-28 11:52:22 -0400

    added function to retrieve weather observation stations and save them to sqlite db

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from requester import Requester
 import pandas as pd
+import sqlite3
 
-def main():
+def retrieve_stations():
     r = Requester()
     data = r.request()
     df = pd.json_normalize(data, record_path="features")
-    print(df)
+    df.columns = df.columns.str.replace("properties.", "", regex=True)
+    df[["xcoord", "ycoord"]] = pd.DataFrame(df["geometry.coordinates"].tolist(), index = df.index)
+    df = df.drop("geometry.coordinates", axis=1)
+    df = df.drop("@id", axis=1)
+    df = df.drop("type", axis=1)
+    
+    conn = sqlite3.connect("db/stations.db")
+    df.to_sql("stations", conn, if_exists="replace", index=False)
+    conn.commit()
+    conn.close()
+
+def main():
+    retrieve_stations()
 
 main()
